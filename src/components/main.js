2befc4df83e0aeabc158fb437071d417c5e5c2b8
Brian O'Carroll <bocarroll36@gmail.com>
2019-03-13 21:09:10 -0700

    added a clear btn

--- src/components/main.js
+++ src/components/main.js
@@ -9,7 +9,9 @@ export default class Draw extends React.Component {
             lineWidth: 5,
             lineCap: 'round',
             strokeStyle: 'red',
-            history: []
+            history: [],
+            tempHistory: [],
+            count: 0
         }
         
         this.mouseX = 0;
@@ -18,7 +20,8 @@ export default class Draw extends React.Component {
         this.mouseDown = this.mouseDown.bind(this);
         this.mouseUp = this.mouseUp.bind(this);
         this.draw = this.draw.bind(this);
-
+        this.clear = this.clear.bind(this);
+        this.undo = this.undo.bind(this)
     }
     componentDidMount() {
         // this.clearBtn()
@@ -53,22 +56,50 @@ export default class Draw extends React.Component {
         });
         console.log(this.state.painting)
     }
+    
     mouseUp() {
-        this.setState({
-            painting: false
-        });
         this.ctx.beginPath();
+        this.setState({
+            painting: false,
+            history: [...this.state.history, this.state.tempHistory],
+        }, () => {
+            this.setState({
+                tempHistory: []
+            })
+        });
+        console.log(this.state.history)
+    }
+    tempHistory(xVal, yVal) {
+        this.setState({
+            tempHistory: [...this.state.tempHistory, {x: xVal, y:yVal}]
+        })
     }
     draw(e) {
+        
+            this.tempHistory(e.nativeEvent.offsetX, e.nativeEvent.offsetY, History)
         if (!this.state.painting) {
             return;
         }
         this.ctx.lineWidth = 5;
         this.ctx.lineCap = 'round'
         this.ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+        // console.log(e.nativeEvent.offsetX, e.nativeEvent.offsetY)
+        // console.log(this.state.tempHistory)
         this.ctx.stroke();
         this.ctx.beginPath();
         this.ctx.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+    }
+    clear() {
+        this.ctx.fillStyle = 'white';
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+    }
+    undo(){
+        let copiedArray = [...this.state.history]
+        let splicedArray = copiedArray.pop()
+        this.setState({
+            history: splicedArray
+        })
+        console.log('spliced', this.state.history)
     }
     //event listeners ////////
 
@@ -77,7 +108,6 @@ export default class Draw extends React.Component {
     //     canvas.addEventListener('mouseup', () => this.mouseUp())
     //     canvas.addEventListener('mousemove', (e) => this.draw(e))
     // }
-    
     onLoad() {
         this.mouseHandler();
 
@@ -86,7 +116,8 @@ export default class Draw extends React.Component {
         return(
             <div>
                 <canvas className="canvas" onMouseUp={this.mouseUp} onMouseDown={this.mouseDown} onMouseMove={(e)=> this.draw(e)} ref="canvas" width={500} height={500}></canvas>
-                <clearBtn ctx={this.ctx} onClick={() => }/>
+                <button onClick={this.clear}></button>
+                <button onClick={this.undo}></button>
             </div>
         )
     }
